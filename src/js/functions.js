f67f1580be161fa7edaa95f8d32b77c127252fa5
Mikhail Ragulin <anarchy16@inbox.ru>
2017-12-25 17:12:54 +0600

    1) add normalize.css into dependencies

---
+++ src/js/functions.js
@@ -0,0 +1,140 @@
+//  Определение класса кнопки для идентификации списка
+export function buttonClass(children) {
+    let result = false;
+
+    for (let i = 0; i < children.length; i++) {
+        if (children[i].classList.contains('filter__item-btn--add')) {
+            children[i].classList.remove('filter__item-btn--add');
+            children[i].classList.add('filter__item-btn--remove');
+            result = true;
+        } else if (children[i].classList.contains('filter__item-btn--remove')) {
+            children[i].classList.remove('filter__item-btn--remove');
+            children[i].classList.add('filter__item-btn--add');
+        }
+    }
+
+    return result;
+}
+//  Перемещение элемента в список выбранных друзей
+export function moveRight(elem, allFriends, container) {
+    container.appendChild(elem);
+
+    for (let i = 0; i < allFriends.length; i++) {
+        if (allFriends[i].id == elem.dataset.id) {
+            allFriends.splice(i, 1);
+        }
+    }
+}
+//  Перемещение элемента из списка выбранных друзей в общий список
+export function moveLeft(elem, resultItems, chooseFriends, container) {
+    container.appendChild(elem);
+
+    for (let i = 0; i < resultItems.length; i++) {
+        if (resultItems[i].id == elem.dataset.id) {
+            chooseFriends.push(resultItems[i]);
+        }
+    }
+}
+//  Формирование списка выбранных друзей
+export function getChooseArray(firstList, resultList) {
+    let chooseArray = [];
+    let arrayOfIndex = [];
+
+    firstList.forEach( (firstItem, i) => {
+        resultList.forEach( (resItem, j) => {
+            if (firstItem.id == resItem.id) {
+                arrayOfIndex.push(i);
+            }
+        });
+    });
+
+    //  Выбранные элементы
+    firstList.forEach( (item, i) => {
+        if (!arrayOfIndex.includes(i)) {
+            chooseArray.push(item);
+        }
+    });
+
+    return chooseArray;
+}
+//  Динамический список drag&drop элементов
+export function getDragElements(container) {
+    let childNodes = container.childNodes;
+    let arrayItems = [];
+
+    for (let i = 0; i < childNodes.length; i++) {
+        if (childNodes[i].nodeName == 'DIV' && childNodes[i].classList.contains('filter__block')) {
+            let children = childNodes[i].children;
+
+            for (let j = 0; j < children.length; j++) {
+                if (children[j].classList.contains('filter__list')) {
+                    let listChildren = children[j].children;
+
+                    for (let x = 0; x < listChildren.length; x++) {
+                        if (listChildren[x].classList.contains('filter__item')) {
+                            arrayItems.push(listChildren[x]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    [].forEach.call(arrayItems, elem => {
+        elem.addEventListener('dragstart', function (event) {
+            event.dataTransfer.effectAllowed = 'move';
+            event.dataTransfer.setData('text', this.dataset.id);
+            return true;
+        });
+    });
+}
+
+//  Рендерим списки
+export function renderTemplate(dataList, initiallyList, list, leftContainer, rightContainer = '') {
+    const Handlebars = require('handlebars');
+    let html = '';
+    let dataListItems = dataList.items;
+    let template = `{{#each items}}
+                        <li class="filter__item" data-id="{{id}}" draggable="true">
+                            <div class="filter__item-photo" style="background: url('{{photo_100}}'); background-size: cover;"></div>
+                            <div class="filter__item-name">{{first_name}} {{last_name}}</div>
+                            <button class="filter__item-btn {{class_name}}"></button>
+                        </li>
+                        {{/each}}`;
+    let render = Handlebars.compile(template);
+
+    if (list === 'all') {
+        dataListItems.forEach( (item) => {
+            item.class_name = 'filter__item-btn--add';
+        });
+
+        html = render(dataList);
+        leftContainer.innerHTML = html;
+    } else if (list === 'choose') {    //  если человек есть в сохранённом списке, то в список выбранных друзей мы его не добавляем
+        dataListItems.forEach( (item, i) => {
+            item.class_name = 'filter__item-btn--add';
+
+            initiallyList.forEach( (item, j, initiallyList) => {
+                item.class_name = 'filter__item-btn--remove';
+
+                if (item.id == dataListItems[i].id) {
+                    initiallyList.splice(j, 1);
+                }
+            });
+        });
+        //  общий список
+        html = render(dataList);
+        leftContainer.innerHTML = html;
+
+        //  список выбранных друзей
+        html = render({items: initiallyList});
+        rightContainer.innerHTML = html
+    } else if (list === 'choose-search') {  //   Поиск по выбранным людям
+        dataListItems.forEach( (item) => {
+            item.class_name = 'filter__item-btn--remove';
+        });
+
+        html = render(dataList);
+        rightContainer.innerHTML = html;
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import { buttonClass, moveRight, moveLeft, getChooseArray, getDragElements, renderTemplate } from './functions';
 import styles from '../css/style.css';
 import html from '../index.html';
 
@@ -28,63 +29,18 @@ new Promise( (resolve, reject) => {
     let allFriendsList = document.querySelector('#all-friends');
     let chooseFriendsList = document.querySelector('#choose-friends');
     let save = document.querySelector('#save');
-    let resultList = result.items;
-    let newAllFriends = [];
     let friends = {};
+    let resultList = JSON.parse(JSON.stringify(result.items));
 
     const firstList = JSON.parse(JSON.stringify(result.items));
 
     if (!localStorage.friends) {
-        renderTemplate(result, 'all');
-        newAllFriends = result.items;
+        renderTemplate(result, firstList, 'all', allFriendsList);
+        getDragElements(wrapper);
     } else {    //  если есть сохранённые списки
-        friends = JSON.parse(localStorage.friends);
-        renderTemplate({ items: friends }, 'choose');
-        newAllFriends = friends;
-    }
-
-    //  Рендерим списки
-    function renderTemplate(data, list) {
-        let template = document.querySelector('#user-template').textContent;
-        let render = Handlebars.compile(template);
-        let html = '';
-        let dataItems = data.items;
-        let resultItems = result.items;
-
-        if (list === 'all') {
-            dataItems.forEach( (item, i, dataItems) => {
-                item.class_name = 'filter__item-btn--add';
-            });
-
-            html = render(data);
-            allFriendsList.innerHTML = html;
-        } else if (list === 'choose') {    //  если человек есть в сохранённом списке, то в список выбранных друзей мы его не добавляем
-            dataItems.forEach( (item, i, dataItems) => {
-                item.class_name = 'filter__item-btn--add';
-
-                resultItems.forEach( (item, j, resultItems) => {
-                    item.class_name = 'filter__item-btn--remove';
-
-                    if (item.id == dataItems[i].id) {
-                        resultItems.splice(j, 1);
-                    }
-                });
-            });
-            //  общий список
-            html = render(data);
-            allFriendsList.innerHTML = html;
-
-            //  список выбранных друзей
-            html = render({items: resultItems});
-            chooseFriendsList.innerHTML = html
-        } else if (list === 'choose-search') {  //   Поиск по выбранным людям
-            dataItems.forEach( (item, i, dataItems) => {
-                item.class_name = 'filter__item-btn--remove';
-            });
-
-            html = render(data);
-            chooseFriendsList.innerHTML = html;
-        }
+        resultList = JSON.parse(localStorage.friends);
+        renderTemplate({ items: resultList }, result.items, 'choose', allFriendsList, chooseFriendsList);
+        getDragElements(wrapper);
     }
 
     // Поиск
@@ -94,7 +50,7 @@ new Promise( (resolve, reject) => {
         if (event.target.id == 'search-all') {
             let all = [];
 
-            newAllFriends.forEach( item => {
+            resultList.forEach( item => {
                 fullName = `${item.first_name} ${item.last_name}`;
 
                 if (fullName.toLowerCase().indexOf(event.target.value.toLowerCase()) != -1) {
@@ -102,28 +58,11 @@ new Promise( (resolve, reject) => {
                 }
             });
 
-            renderTemplate({ items: all }, 'all');
+            renderTemplate({ items: all }, firstList, 'all', allFriendsList);
+            getDragElements(wrapper);
         } else {
-            let chooseArray = [];
-            let arrayOfIndex = [];
+            let chooseArray = getChooseArray(firstList, resultList);
             let newChooseArray = [];
-
-
-            if (!localStorage.friends) {
-                firstList.forEach( (firstItem, i) => {
-                    resultList.forEach( (resItem, j) => {
-                        if (firstItem.id == resItem.id) {
-                            arrayOfIndex.push(i);
-                        }
-                    });
-                });
-                //  Выбранные элементы
-                firstList.forEach( (item, i) => {
-                    if (!arrayOfIndex.includes(i)) {
-                        chooseArray.push(item);
-                    }
-                });
-            }
 
             chooseArray.forEach(item => {
                 fullName = `${item.first_name} ${item.last_name}`;
@@ -133,7 +72,8 @@ new Promise( (resolve, reject) => {
                 }
             });
 
-            renderTemplate({ items: newChooseArray }, 'choose-search');
+            renderTemplate({ items: newChooseArray }, firstList, 'choose-search', allFriendsList, chooseFriendsList);
+            getDragElements(wrapper);
         }
     });
 
@@ -145,25 +85,14 @@ new Promise( (resolve, reject) => {
                 event.target.classList.remove('filter__item-btn--add');
                 event.target.classList.add('filter__item-btn--remove');
 
-                newAllFriends = moveRight(parent, newAllFriends);
+                moveRight(parent, resultList, chooseFriendsList);
             } else {
                 event.target.classList.remove('filter__item-btn--remove');
                 event.target.classList.add('filter__item-btn--add');
 
-                friends = moveLeft(parent, result.items, friends);
+                moveLeft(parent, firstList, resultList, allFriendsList);
             }
         }
-    });
-
-    let dragElements = wrapper.querySelectorAll('.filter__item');
-
-    [].forEach.call(dragElements, elem => {
-        elem.addEventListener('dragstart', function (event) {
-            event.dataTransfer.effectAllowed = 'move';
-            event.dataTransfer.setData('text', this.dataset.id);
-
-            return true;
-        });
     });
 
     wrapper.addEventListener('dragenter', event => {
@@ -184,62 +113,17 @@ new Promise( (resolve, reject) => {
         let list = buttonClass(elem.children);
 
         if (list) {
-            newAllFriends = moveRight(elem, newAllFriends);
+            moveRight(elem, resultList, chooseFriendsList);
         } else {
-            friends = moveLeft(elem, result.items, friends);
+            moveLeft(elem, firstList, resultList, allFriendsList);
         }
     });
 
     //  Сохранение данных
     save.addEventListener('click', () => {
-        localStorage.friends = JSON.stringify(newAllFriends);
+        localStorage.friends = JSON.stringify(resultList);
         alert('Данные сохранены!')
     });
 }).catch((error) => {
     alert('Ошибка ' + error.message);
 });
-//  Определение класса кнопки для идентификации списка
-function buttonClass(children) {
-    let result = false;
-
-    for (let i = 0; i < children.length; i++) {
-        if (children[i].classList.contains('filter__item-btn--add')) {
-            children[i].classList.remove('filter__item-btn--add');
-            children[i].classList.add('filter__item-btn--remove');
-            result = true;
-        } else if (children[i].classList.contains('filter__item-btn--remove')) {
-            children[i].classList.remove('filter__item-btn--remove');
-            children[i].classList.add('filter__item-btn--add');
-        }
-    }
-
-    return result;
-}
-//  Перемещение элемента в список выбранных друзей
-function moveRight(elem, allFriends) {
-    let chooseFriends = document.querySelector('#choose-friends');
-
-    chooseFriends.appendChild(elem);
-
-    for (let i = 0; i < allFriends.length; i++) {
-        if (allFriends[i].id == elem.dataset.id) {
-            allFriends.splice(i, 1);
-        }
-    }
-
-    return allFriends;
-}
-//  Перемещение элемента из списка выбранных друзей в общий список
-function moveLeft(elem, resultItems, friends) {
-    let allFriends = document.querySelector('#all-friends');
-
-    allFriends.appendChild(elem);
-
-    for (let i = 0; i < resultItems.length; i++) {
-        if (resultItems[i].id == elem.dataset.id) {
-            friends.push(resultItems[i]);
-        }
-    }
-
-    return friends;
-}
